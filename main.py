772908fa2c699e4100cd34e0f4eb775ef8a1ec15
bethmij <bethmij@gmail.com>
2024-04-06 00:01:58 +0530

    modify word setting

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pandas
 BACKGROUND_COLOR = "#B1DDC6"
 random_choice = {}
 timer = ""
+word_list = {}
 
 window = Tk()
 window.title("Flash-cards")
@@ -13,16 +14,16 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 
 def set_french_word():
-    global random_choice, timer
+    global random_choice, timer, word_list
     try:
         data = pandas.read_csv("data/french_words.csv")
     except FileNotFoundError:
         pass
     else:
-        french_list = data.to_dict(orient="records")
+        word_list = data.to_dict(orient="records")
         canvas.itemconfig(bg_img, image=image_front)
         canvas.itemconfig(label_title, text="French", fill="black")
-        random_choice = random.choice(french_list)
+        random_choice = random.choice(word_list)
         canvas.itemconfig(label_word, text=random_choice['French'], fill="black")
 
     timer = window.after(3000, set_english_word)
@@ -36,6 +37,14 @@ def set_english_word():
     window.after_cancel(timer)
 
 
+def correct_word():
+    global word_list, random_choice
+
+    for index, word in enumerate(word_list):
+        if word['French'] == random_choice['French']:
+            del word_list[index]
+
+
 canvas = Canvas(bg=BACKGROUND_COLOR, width=900, height=560, highlightthickness=0)
 image_back = PhotoImage(file="images/card_back.png", width=800, height=530)
 image_front = PhotoImage(file="images/card_front.png", width=800, height=530)
@@ -45,7 +54,7 @@ label_word = canvas.create_text(450, 300, text="", font=("Arial", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 image3 = PhotoImage(file="images/wrong.png")
-button1 = Button(image=image3, highlightthickness=0, command=set_french_word)
+button1 = Button(image=image3, highlightthickness=0, command=correct_word)
 button1.grid(row=1, column=0)
 
 image4 = PhotoImage(file="images/right.png")
