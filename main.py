edd485629d3b6b78db2e3edc0779824d1922fa9f
bethmij <bethmij@gmail.com>
2024-04-05 22:40:44 +0530

    modify French words

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ def set_french_word():
     except FileNotFoundError:
         pass
     else:
-        french_list = data.to_dict()
-        canvas2.itemconfig(label_french, text=random.choice(french_list['French']))
+        french_list = data.to_dict(orient="records")
+        canvas2.itemconfig(label_title, text="French")
+        canvas2.itemconfig(label_word, text=random.choice(french_list)['French'])
 
 
 canvas1 = Canvas(bg=BACKGROUND_COLOR, width=900, height=650, highlightthickness=0)
@@ -28,8 +29,8 @@ canvas1.grid(row=0, column=0, columnspan=2)
 canvas2 = Canvas(bg=BACKGROUND_COLOR, width=900, height=650, highlightthickness=0)
 image2 = PhotoImage(file="images/card_front.png", width=800, height=530)
 canvas2.create_image(450, 280, image=image2)
-canvas2.create_text(450, 150, text="French", font=("Arial", 40, "italic"))
-label_french = canvas2.create_text(450, 300, text="english", font=("Arial", 60, "bold"))
+label_title = canvas2.create_text(450, 150, text="", font=("Arial", 40, "italic"))
+label_word = canvas2.create_text(450, 300, text="", font=("Arial", 60, "bold"))
 canvas2.grid(row=0, column=0, columnspan=2)
 
 image3 = PhotoImage(file="images/wrong.png")
@@ -39,5 +40,7 @@ button1.grid(row=1, column=0)
 image4 = PhotoImage(file="images/right.png")
 button2 = Button(image=image4, highlightthickness=0, command=set_french_word)
 button2.grid(row=1, column=1)
+
+set_french_word()
 
 window.mainloop()
